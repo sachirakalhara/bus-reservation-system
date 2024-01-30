1caa6159e31474e4792f790c52315def3ad968d5
sachiraKalhara <sachira.kalhara.2@gmail.com>
2024-01-31 01:30:19 +0530

    Ui creating

--- resources/views/admin/message.blade.php
+++
@@ -1,9 +0,0 @@
-@if ( Session::has('msg') )
-            <p class="alert alert-info">{{ Session::get('msg') }}</p>
-          @endif
-
-          @if ( count($errors) > 0 ) 
-            @foreach ( $errors->all() as $error ) 
-              <p class="alert alert-danger">{{ $error }}</p>
-            @endforeach
-          @endif
--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,59 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\BusController;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ApiBusController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\OperatorController;
-use App\Http\Controllers\RegionController;
-use App\Http\Controllers\Sub_RegionController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\ChatController;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('home', [HomeController::class, 'index'])->name('home.index');
+Route::get('/bus/search', [HomeController::class, 'search'])->name('home.search');
+Route::get('/bus/{bus}', [HomeController::class, 'show'])->name('home.show');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [AccountController::class, 'profile'])->name('account.profile');
+    Route::get('/logout', [AccountController::class, 'logout'])->name('account.logout');
+    Route::put('/changePassword', [AccountController::class, 'changePassword'])->name('account.changePassword');
+
+    Route::get('/my-reservations', [ReservationController::class, 'index'])->name('reservation.index');
+    Route::post('/bus/applyReservation/{bus}', [ReservationController::class, 'create'])->name('reservation.create');
+    Route::get('/my-reservations/{id}/destroy', [ReservationController::class, 'destroy'])->name('reservation.destroy');
+    Route::get('/my-reservations-api', [ReservationController::class, 'myReservationsApi'])->name('reservation.myReservationsApi');
+
+    //chats 
+    Route::get('/chats', [ChatController::class, 'index'])->name('chat.index');
+    Route::post('/chats', [ChatController::class, 'store'])->name('chat.store');
+    Route::get('/get-messages', [ChatController::class, 'getMessages'])->name('chat.getMessages'); //for users
+    //admin chats
+    Route::get('/get-customer-messages/{userId}', [ChatController::class, 'getCustomerMessages'])->name('chat.getCustomerMessages');
+    Route::get('/get-customer-list', [ChatController::class, 'getCustomerList'])->name('chat.customerList');
 });
 
-Auth::routes();
+Route::prefix('admin')->middleware('role:admin')->group(function () {
+    Route::get('dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('buses', [AdminController::class, 'buses'])->name('admin.buses');
+    Route::get('customers', [CustomerController::class, 'index'])->name('customer.index');
+    Route::get('pending-request', [CustomerController::class, 'pendingRequest'])->name('customer.pendingRequest');
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::Resource('/operator', OperatorController::class);
-Route::Resource('/bus', BusController::class);
-Route::Resource('/region', RegionController::class);
-Route::Resource('/sub-region', Sub_RegionController::class);
+    //admin api routes
+    Route::get('/get-all-buses', [ApiBusController::class, 'getAllBuses'])->name('bus.all');
+    Route::get('/get-all-customers', [CustomerController::class, 'all'])->name('customer.all');
+    Route::get('/get-all-pending-request', [CustomerController::class, 'pendingRequestApi'])->name('customer.pendingRequestApi');
 
-// Operator Routes
-// Route::Resource('operator', 'OperatorController');
+    Route::get('/get-all-notifications', [AdminController::class, 'notifications'])->name('admin.notifications');
 
-//  Bus Route
-// Route::Resource('bus','BusController');
+    //pending request
+    Route::get('/customer-reservations/{id}/approve', [CustomerController::class, 'requestApprove'])->name('bus.requestApprove');
+    Route::get('/customer-reservations/{id}/reject', [CustomerController::class, 'requestReject'])->name('bus.requestReject');
 
-//  Region Route
-// Route::Resource('region','RegionController');
-
-//  Sub Region Route
-// Route::Resource('sub-region','Sub_RegionController');
+    Route::get('/bus/create', [BusController::class, 'create'])->name('bus.create');
+    Route::post('/bus', [BusController::class, 'store'])->name('bus.store');
+    Route::get('/bus/{id}/edit', [BusController::class, 'edit'])->name('bus.edit');
+    Route::put('/bus/{id}', [BusController::class, 'update'])->name('bus.update');
+    Route::delete('/bus,{id}', [BusController::class, 'destroy'])->name('bus.destroy');
+});
